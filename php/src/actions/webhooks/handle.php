d4b2ac6cdceb6a933b8be266377c04e079ab230b
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-06-09 20:01:55 +0300

    Update validation of signature

--- php/src/actions/webhooks/handle.php
+++ php/src/actions/webhooks/handle.php
@@ -1,18 +1,18 @@
 <?php
 
 use Helpers\KafkaHelper;
-use HubSpot\Utils\Webhooks;
+use HubSpot\Utils\Signature;
 
 $requestBody = file_get_contents('php://input');
 
-if (!Webhooks::isHubspotSignatureValid(
-    $_SERVER['HTTP_X_HUBSPOT_SIGNATURE'],
-    $_ENV['HUBSPOT_CLIENT_SECRET'],
-    $requestBody,
-    $_SERVER['REQUEST_URI'],
-    $_SERVER['REQUEST_METHOD'],
-    $_SERVER['HTTP_X_HUBSPOT_SIGNATURE_VERSION']
-)) {
+if (!Signature::isValid([
+    'signature' => $_SERVER['HTTP_X_HUBSPOT_SIGNATURE'],
+    'secret' => $_ENV['HUBSPOT_CLIENT_SECRET'],
+    'requestBody'=> $requestBody,
+    'httpUri' => $_SERVER['REQUEST_URI'],
+    'httpMethod' => $_SERVER['REQUEST_METHOD'],
+    'signatureVersion' => $_SERVER['HTTP_X_HUBSPOT_SIGNATURE_VERSION']
+])) {
     header('HTTP/1.1 401 Unauthorized');
 
     exit();
