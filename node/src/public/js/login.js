9bafe6438ebdd3a79c57ae2fa46effc9e7ba4a74
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-07-20 16:52:40 +0300

    Clear login js

--- node/src/public/js/login.js
+++ node/src/public/js/login.js
@@ -4,16 +4,3 @@ document.getElementById("copyBtn").onclick = async() => {
     
     alert('Copied')
 }
-async function copyRedirectURL() {
-    let field = document.createElement('input')
-    field.value = document.getElementById('redirectURL').textContent
-    let text = document.getElementById('redirectURL').textContent
-    await navigator.clipboard.writeText(text)
-    // document.body.appendChild(field)
-    // field.select()
-    
-    // document.execCommand('copy')
-    // document.body.removeChild(field)
-    
-    alert('Copied')
-}
